c538dad417b6e2d307df48df3a071b7dd933b559
Yasmim-CR <yas.castr0.r@gmail.com>
2025-05-30 09:00:26 -0300

    logica tabuada

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+int numero;
+
+Console.Write("Digite um número para realizar a tabuada: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+for(int n = 0; n <= 10; n++){
+    Console.WriteLine($"{numero} * {n} = {numero * n}");
+}
